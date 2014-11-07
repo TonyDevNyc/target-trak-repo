1d09e0f37756406eb4f175252956075394239e1a
Tony M <tonydevgroupny@gmail.com>
2014-11-07 16:42:15 -0500

    added extJS to web project

---
+++ target-trak-system/target-trak-web/src/main/webapp/app/view/Viewport.js
@@ -0,0 +1,12 @@
+Ext.define('TGT.view.Viewport', {
+    extend : 'Ext.container.Viewport',
+    requires : [ 
+        'Ext.layout.container.Border'
+    ],
+    layout : {
+        type : 'border'
+    },
+    items : [ 
+        
+    ]
+});
